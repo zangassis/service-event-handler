23ef71d75dda933bac642307f3adb7b360c311c5
assiszang <assiszang@gmail.com>
2024-07-26 09:14:09 -0300

    feat: Add settings

--- Program.cs
+++ Program.cs
@@ -9,15 +9,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEnyimMemcached(options =>
 {
-    options.Servers.Add(new Enyim.Caching.Configuration.Server
-    {
-        Address = "localhost",
-        Port = 11211
-    });
+	options.Servers.Add(new Enyim.Caching.Configuration.Server
+	{
+		Address = "localhost",
+		Port = 11211
+	});
 });
 
 builder.Services.AddMemoryCache();
@@ -26,8 +26,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -38,63 +38,65 @@ app.MapControllers();
 
 InitializeDatabase(app);
 
+app.UseEnyimMemcached();
+
 app.Run();
 
 static void InitializeDatabase(WebApplication app)
 {
-    using var scope = app.Services.CreateScope();
-    var services = scope.ServiceProvider;
-    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+	using var scope = app.Services.CreateScope();
+	var services = scope.ServiceProvider;
+	var dbContext = services.GetRequiredService<ApplicationDbContext>();
 
-    if (dbContext.Database.EnsureCreated())
-    {
-        if (!dbContext.Services.Any())
-        {
-            var servicesToAdd = GenerateSampleServices(100);
-            dbContext.Services.AddRange(servicesToAdd);
-            dbContext.SaveChanges();
-        }
+	if (dbContext.Database.EnsureCreated())
+	{
+		if (!dbContext.Services.Any())
+		{
+			var servicesToAdd = GenerateSampleServices(100);
+			dbContext.Services.AddRange(servicesToAdd);
+			dbContext.SaveChanges();
+		}
 
-        if (!dbContext.Logs.Any())
-        {
-            var logsToAdd = GenerateSampleLogs(dbContext.Services.ToList(), 1000);
-            dbContext.Logs.AddRange(logsToAdd);
-            dbContext.SaveChanges();
-        }
-    }
+		if (!dbContext.Logs.Any())
+		{
+			var logsToAdd = GenerateSampleLogs(dbContext.Services.ToList(), 1000);
+			dbContext.Logs.AddRange(logsToAdd);
+			dbContext.SaveChanges();
+		}
+	}
 }
 
 static List<Service> GenerateSampleServices(int count)
 {
-    var services = new List<Service>();
+	var services = new List<Service>();
 
-    for (int i = 1; i <= count; i++)
-    {
-        services.Add(new Service
-        {
-            Name = $"Service {i}"
-        });
-    }
-    return services;
+	for (int i = 1; i <= count; i++)
+	{
+		services.Add(new Service
+		{
+			Name = $"Service {i}"
+		});
+	}
+	return services;
 }
 
 static List<Log> GenerateSampleLogs(List<Service> services, int countPerService)
 {
-    var logs = new List<Log>();
-    var random = new Random();
+	var logs = new List<Log>();
+	var random = new Random();
 
-    foreach (var service in services)
-    {
-        for (int i = 1; i <= countPerService; i++)
-        {
-            logs.Add(new Log
-            {
-                Service = service,
-                Message = $"Log message {i} for {service.Name}",
-                LogLevel = (InternalLogLevel)random.Next(0, 3),
-                Timestamp = DateTime.UtcNow.AddHours(-random.Next(1, 1000))
-            });
-        }
-    }
-    return logs;
+	foreach (var service in services)
+	{
+		for (int i = 1; i <= countPerService; i++)
+		{
+			logs.Add(new Log
+			{
+				Service = service,
+				Message = $"Log message {i} for {service.Name}",
+				LogLevel = (InternalLogLevel)random.Next(0, 3),
+				Timestamp = DateTime.UtcNow.AddHours(-random.Next(1, 1000))
+			});
+		}
+	}
+	return logs;
 }
